a0821cd9bb682ac9246069e4e140bda95b454d03
Lauani Vieira <vieira.lauani@escola.pr.gov.br>
2025-09-23 10:30:20 -0300

    23/09

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,52 @@
 Criacartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Matematica',
+    'Quantas patas uma formiga possui? ',
+    '6'
 )
-
 Criacartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Historia',
+    'Quantos anos tem um século?',
+    '100'
+)
+Criacartao(
+    'Geografia'
+    'Qual o nome do único satélite natural da terra?
+',
+    'lua'
+)
+Criacartao(
+    'geografia'
+    'Qual é o maior pais da América do sul?
+',
+    'Brasil'
+)
+Criacartao(
+    'Arte',
+    'Quem pintou o quadro Mona Lisa?',
+    'Aeonardo da vinci'
+)
+Criacartao(
+    'ciencia',
+    'Que nome se dá para o coletivo de lobos?',
+    'Alcateia'
+)
+Criacartao(
+    'Geografia',
+    'Como é o nome da energia gerada pelo vento?',
+    'Eólica'
+)
+Criacartao(
+    'Biologia',
+    'Qual é a fórmula da água?',
+    'H2O'
+)
+Criacartao(
+    'Historia',
+    'Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?',
+    'Titanic'
+)
+Criacartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'Madri'
 )
